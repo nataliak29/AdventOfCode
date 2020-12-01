96dbf199df5d1985db2e4efe9122744b640e34fc
Natalia Kukushkina <natkk@amazon.com>
2020-12-01 18:53:41 -0500

    removed duplicated loops

--- day01/sumTwoThree.py
+++ day01/sumTwoThree.py
@@ -10,9 +10,12 @@ def sum_of_two(input_data, target_int):
     for i in range(len(input_data)):
         for j in range(len(input_data)):
             if input_data[i]+input_data[j] == target_int:
-                print("Numbers are ", input_data[i],input_data[j])
+                print("Numbers that sum up to",target_int,"are", input_data[i],input_data[j])
                 print("Answer for 2 number sum is", input_data[i]*input_data[j])
                 break
+        else:
+            continue
+        break
         
 
 def sum_of_three(input_data, target_int): 
@@ -20,9 +23,15 @@ def sum_of_three(input_data, target_int):
         for j in range(len(input_data)):
             for k in range(len(input_data)):
                 if input_data[i]+input_data[j]+ input_data[k] == target_int:
-                    print("Numbers are ", input_data[i],input_data[j],input_data[k] )
+                    print("Numbers that sum up to",target_int,"are", input_data[i],input_data[j],input_data[k] )
                     print("Answer for 3 number sum is", input_data[i]*input_data[j]*input_data[k] )
                     break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
             
 
 sum_of_two(data,2020)
