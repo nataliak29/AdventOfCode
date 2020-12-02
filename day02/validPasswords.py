bc51dcc55e910e93ba0c1c346b68152646c46545
Natalia Kukushkina <natkk@amazon.com>
2020-12-02 10:01:21 -0500

    code for the part 2

--- day02/validPasswords.py
+++ day02/validPasswords.py
@@ -17,19 +17,36 @@ def is_password_valid(password,policy_letter,min_frq,max_frq):
     else:
         return 0
 
+def is_password_valid_toboggan(password,policy_letter,start,end):
+    pos_letter_count=0
+    start_pos=start-1
+    end_pos=end-1
+    if password[start_pos:start_pos+1]==policy_letter:
+        pos_letter_count+=1
+    if password[end_pos:end_pos+1]==policy_letter:
+        pos_letter_count+=1
+    if pos_letter_count==1:
+        return 1
+    else:
+        return 0
+
 
 def valid_passwords(input_data):
     valid_pass_count=0 
+    valid_pass_count_toboggan=0 
     for i in range(len(input_data)):
-        min_frq=int(input_data[i][0].split('-')[0])
-        max_frq=int(input_data[i][0].split('-')[1])
+        policy_start=int(input_data[i][0].split('-')[0])
+        policy_end=int(input_data[i][0].split('-')[1])
         policy_letter=input_data[i][1][:1]
         password = input_data[i][2]
-        valid_pass_count+=is_password_valid(password,policy_letter,min_frq,max_frq)
+        valid_pass_count+=is_password_valid(password,policy_letter,policy_start,policy_end)
+        valid_pass_count_toboggan+=is_password_valid_toboggan(password,policy_letter,policy_start,policy_end)
         print("Processing",input_data[i])
-        print("Is password valid (1/0):",is_password_valid(password,policy_letter,min_frq,max_frq))
+        print("Is password valid (1/0):",is_password_valid(password,policy_letter,policy_start,policy_end))
+        print("Is password valid based on Toboggan policy (1/0):",is_password_valid_toboggan(password,policy_letter,policy_start,policy_end))
             
     print("Total number of valid passwords",valid_pass_count)
+    print("Total number of valid passwords on Toboggan policy",valid_pass_count_toboggan)
 
 
 
