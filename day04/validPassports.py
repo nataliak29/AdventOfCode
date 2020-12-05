ce36d33a5a82c790db544c6cfe302ec4cab80b1a
Natalia Kukushkina <natkk@amazon.com>
2020-12-04 20:08:11 -0500

    part 2 code

--- day04/validPassports.py
+++ day04/validPassports.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 def get_data(filename):
     folder = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(folder, filename)
@@ -37,10 +37,55 @@ def is_valid_passp(passport_entry):
             continue
     if req_count==7:
         return 1
-
     else:
         return 0
 
+def is_valid_passp_advanced(passport_entry):
+    req_count=0
+    for item in passport_entry.split(' '):
+        item.split(':')
+        req=item.split(':')[0]
+        req_value=item.split(':')[1]
+
+        if req =='byr':
+            if int(req_value)>=1920 and int(req_value)<=2002:
+                req_count+=1
+        
+        if req =='iyr':
+            if int(req_value)>=2010 and int(req_value)<=2020:
+                req_count+=1
+        
+        if req =='eyr':
+            if int(req_value)>=2020 and int(req_value)<=2030:
+                req_count+=1
+
+        if req =='hgt':
+            if 'cm' in req_value:
+                height = int(re.findall(r'\d+', req_value)[0])
+                if height >=150 and height<=193:
+                    req_count+=1
+            elif 'in' in req_value:
+                height = int(re.findall(r'\d+', req_value)[0])
+                if height >=59 and height<=76:
+                    req_count+=1
+
+        if req =='hcl':
+            if req_value[0]=="#":
+                if req_value[1:].isalnum()==True and len(req_value[1:])==6:
+                    req_count+=1
+
+        if req =='ecl':
+            if req_value in ['amb','blu','brn', 'gry', 'grn', 'hzl', 'oth']:
+                req_count+=1
+
+        if req =='pid':
+            if req_value.isnumeric()==True and len(req_value)==9:
+                req_count+=1
+
+    if req_count==7:
+        return 1
+    else:
+        return 0
 
 data=get_data('input.txt')
 data_joined=join_data(data)
@@ -48,6 +93,6 @@ count_valid_passports=0
 for i in range(len(data_joined)):
     passport_entry=[]
     passport_entry=data_joined[i][0]
-    count_valid_passports+=is_valid_passp(passport_entry)
+    count_valid_passports+=is_valid_passp_advanced(passport_entry)
    
 print('Total count of valid passports',count_valid_passports)
