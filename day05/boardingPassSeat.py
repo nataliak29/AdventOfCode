eb739f4c2c6bd2ad360cbc68d41e0f8d2a8d8500
Natalia Kukushkina <natkk@amazon.com>
2020-12-05 14:51:51 -0500

    code for part 2

--- day05/boardingPassSeat.py
+++ day05/boardingPassSeat.py
@@ -1,3 +1,5 @@
+#Instructions:https://adventofcode.com/2020/day/5
+
 import os
 import math 
 
@@ -47,15 +49,12 @@ def run_tests():
         row=seat_row(entry)
         column=seat_column(entry)
         seat_id=row*8+column
-        print(row, column)
         if row==test_row[i] and column==test_column[i] and seat_id==test_seat_id[i]:
             print("Test passed for", entry)
         else:
             print("Test failed for", entry)
 
-
 def max_seat_id(filename):
-
     data=get_data(filename)
     max_seat_id=0
     for i in range(len(data)):
@@ -69,6 +68,46 @@ def max_seat_id(filename):
     return max_seat_id
 
 
+def seat_ids_in_file(filename):
+    seat_ids=[]
+    data=get_data(filename)
+    for i in range(len(data)):
+        entry=data[i]
+        row=seat_row(entry)
+        column=seat_column(entry)
+        seat_id=row*8+column
+        seat_ids.append(seat_id)
+    return seat_ids
+
+def max_min_seat_id(seat_ids):
+    max_seat_id=0
+    min_seat_id=127
+    for i in range(len(seat_ids)):
+        seat_id=seat_ids[i]
+        if seat_id<min_seat_id:
+            min_seat_id=seat_id
+        if seat_id>max_seat_id:
+            max_seat_id=seat_id
+                  
+    return min_seat_id,max_seat_id
+
+def my_seat_id(seat_ids):
+    min_seat_id=max_min_seat_id(seat_ids)[0]
+    max_seat_id= max_min_seat_id(seat_ids)[1]
+    my_seat_id=0
+    for i in range(min_seat_id,max_seat_id):
+        if i not in seat_ids:
+            print("seat_id",i,"is NOT in the file")
+            my_seat_id=i
+        else:
+            continue
+    return my_seat_id
+
+
 run_tests()
 print("Max seat id is:",max_seat_id("input.txt"))
+seat_ids=seat_ids_in_file("input.txt")
+print("My seat_is is:",my_seat_id(seat_ids))
+
+
 
