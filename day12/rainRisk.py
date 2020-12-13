772915fb56f93a22a6d54f927c1a632089e6be82
Natalia Kukushkina <natkk@amazon.com>
2020-12-12 19:37:02 -0500

    code for part 2

--- day12/rainRisk.py
+++ day12/rainRisk.py
@@ -39,13 +39,49 @@ def ship_position(data):
     return position
         
 def manth_dist(position):
-    manth_dist=abs(position["E"]-position["W"])+abs(position["N"]-position["S"])
+    try:
+        manth_dist=abs(position["E"]-position["W"])+abs(position["N"]-position["S"])
+    except:
+        manth_dist=abs(position["E"])+abs(position["N"])
     return manth_dist
+
+def ship_position_with_waypoint(data):
+
+    s_position ={"N":0,"E":0}
+    w_position ={"N":1,"E":10}
+
+    for instr in data:
+    
+        action=instr[0]
+        value=int(instr[1:])
+        diff_n=w_position.get("N")-s_position.get("N")
+        diff_e=w_position.get("E")-s_position.get("E")
+        if action in ["N","E"]:
+            w_position[action]=w_position.get(action)+value
+        if action =="S":
+            w_position["N"]=w_position.get("N")-value
+        if action =="W":
+            w_position["E"]=w_position.get("E")-value
+        if action=="F":
+            s_position["N"]=(w_position.get("N")-s_position.get("N"))*value+s_position["N"]
+            s_position["E"]=(w_position.get("E")-s_position.get("E"))*value+s_position["E"]
+            w_position["N"]=s_position["N"]+diff_n
+            w_position["E"]=s_position["E"]+diff_e
+        if instr=="R90" or instr=="L270":
+            w_position["N"]=s_position["N"]-diff_e
+            w_position["E"]=s_position["E"]+diff_n
+        if instr=="R180" or instr=="L180":
+            w_position["N"]=s_position["N"]-diff_n
+            w_position["E"]=s_position["E"]-diff_e
+        if instr=="R270"  or instr=="L90":
+            w_position["N"]=s_position["N"]+diff_e
+            w_position["E"]=s_position["E"]-diff_n
+    return s_position
+    
 
 
 data=get_data("input.txt")
-position=ship_position(data)
-print("answer",position)
-print(manth_dist(position))
+print("Part 1 answer:",manth_dist(ship_position(data)))
+print("Part 2 answer:",manth_dist(ship_position_with_waypoint(data)))
 
 
