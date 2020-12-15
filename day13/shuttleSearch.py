7fb72b45a2bbd8b00da65b4a44f6375fc9efbc98
Natalia Kukushkina <natkk@amazon.com>
2020-12-14 19:36:51 -0500

    code for part 2

--- day13/shuttleSearch.py
+++ day13/shuttleSearch.py
@@ -28,12 +28,38 @@ def next_buses_dict(data,start_time):
             next_buses[bus]=[when_is_next_bus,diff]
     return next_buses
 
-data=get_data("input.txt")
+data=get_data("test.txt")
 start_time=int(data[0])
 next_buses=next_buses_dict(data,start_time)
 next_bus = min(next_buses.keys(), key=(lambda k: next_buses[k]))
 print("Answer for part 1",next_bus*next_buses[next_bus][1])
 
+
+
+def find_timest(data):
+
+    buses=data[1].split(",")
+    buses_dict={}
+    for i,bus in enumerate(buses):
+        if bus!='x':
+            buses_dict[int(bus)]=i
+    #print(buses_dict)
+    start, step = 0, 1
+    for id, delta in buses_dict.items():
+        #print(id,delta)
+        for guess in range(start, step * id, step):
+            #print("start",start,"step*id",step*id,"step",step)
+            #print ("guess",guess)
+            if (guess + delta) % id == 0:
+                step *= id
+                start = guess
+                #print("condition satisf",guess)
+        if id==list(buses_dict)[-1]:
+            return guess
+        
+     
+
+print("Part 2 answer:",find_timest(data))
 
 
 
