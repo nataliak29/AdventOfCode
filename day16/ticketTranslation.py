2b64c4bd26400a062cfe7ee5935b9fce679cf909
Natalia Kukushkina <natkk@amazon.com>
2020-12-20 11:58:48 -0500

    code for part 2

--- day16/ticketTranslation.py
+++ day16/ticketTranslation.py
@@ -19,7 +19,6 @@ def parse_rules(rules):
     for rule in rules:
         inst=rule.split(':')[0]
         values=rule.split(': ')[1]
-        #print(inst,values)
         order=1        
         for val in values.split(' or '):
             values_lst=[]
@@ -56,9 +55,80 @@ def check_tickets(nearby_tickets):
     return err_rate
 
 
+def valid_tickets(nearby_tickets):
+    invalid_tickets=[]
+    valid_tickets_list=[]
+    for ticket in nearby_tickets:
+        for entry in ticket:
+            if ticket_invalid_entry(entry)==False:
+                invalid_tickets.append(ticket)
+    valid_tickets_list= [x for x in nearby_tickets if x not in invalid_tickets]
+            
+    return valid_tickets_list
+
+def find_entry_name(col_num):
+    name_dict={}
+    for tick in valid_nearby_tickets:
+        tick_entry=tick[col_num]
+
+        for key in rules:        
+            min_k=rules[key][0]
+            max_k=rules[key][1]
+            if tick_entry<=max_k and tick_entry>=min_k:
+                try:
+                    name_dict[key[:-1]]= name_dict.get(key[:-1])+1
+                except:
+                    name_dict[key[:-1]]=1
+    return name_dict
+
+
+
+
 rules_raw,raw_your_ticket,raw_nearby_tickets=get_data("input.txt")
 rules= parse_rules(rules_raw)
 nearby_tickets=parse_nearby_tickets(raw_nearby_tickets)
 your_ticket=parse_nearby_tickets(raw_your_ticket)
-
 print("Part 1 answer:",check_tickets(nearby_tickets))
+
+valid_nearby_tickets=valid_tickets(nearby_tickets)
+len_rules=len(rules)/2
+
+
+def decode_tickets():
+    final_dict={}
+    for col in range(len(your_ticket[0])):
+        name_dict=find_entry_name(col)
+        d=[]
+        for k in name_dict:
+            if name_dict[k]==len(valid_nearby_tickets):
+                d.append(k)
+        final_dict[col]=d
+    
+    found=[]
+    d={}
+    i=0
+    while  i<len_rules:
+    
+        for k in final_dict:
+            if len(final_dict[k])==1:
+                found.append(final_dict[k][0])
+                d[k]=final_dict[k][0]
+    
+        for k in final_dict:
+            this_d=final_dict[k]
+            this_d_n= [x for x in this_d if x not in found]
+            final_dict[k]=this_d_n
+        i+=1
+    return d
+
+decoded=decode_tickets()
+final_product=1
+for k,v in decoded.items():
+    if decoded[k][:9]=="departure":
+        final_product=final_product*your_ticket[0][k]
+
+print("Part 2 answer:",final_product)
+
+
+
+           
